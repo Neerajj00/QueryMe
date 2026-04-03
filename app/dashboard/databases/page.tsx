b5930da8d3242380087a8e1e8091c79500976089
Neeraj-gupta2005 <neerajKgupta33@gmail.com>
2026-04-03 16:07:02 +0530

    made a final layout for dashboard

--- app/dashboard/databases/page.tsx
+++ app/dashboard/databases/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function page() {
   return (
-    <div>chat query page</div>
+    <div>databases page</div>
   )
 }
 
---
+++ app/dashboard/history/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function history() {
+  return (
+    <div>history page</div>
+  )
+}
+
+export default history
