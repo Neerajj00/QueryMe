95398f217582cdb570653010e93e9adc518b3131
Neeraj-gupta2005 <neerajKgupta33@gmail.com>
2026-04-05 22:30:24 +0530

    added databade adding dialog component

--- app/dashboard/databases/page.tsx
+++ app/dashboard/databases/page.tsx
@@ -2,77 +2,21 @@ import { Button } from "@/components/ui/button";
 import React from "react";
 import { Plus } from "lucide-react";
 import { DatabaseCard } from "./DatabaseCard";
+import { AddDatabaseDialog } from './AddDatabadeDialog';
 
 function page() {
   return (
     <div className="h-full w-full flex flex-col overflow-hidden">
       {/* top fixed */}
-      <div className="flex w-full h-16 justify-end shrink-0">
-        <Button variant="default">
-          <Plus />
-          Add Database
-        </Button>
+      <div className=" w-full h-16 justify-end shrink-0 flex">
+        <AddDatabaseDialog />
       </div>
 
       {/* scrollable area */}
       <div className="flex-1 overflow-y-auto mt-2 ">
         <div className=" w-full grid  sm:grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-4">
         
-        <DatabaseCard 
-        name = "My Database"
-        type = "PostgreSQL"
-        createdAt = "2024-06-01"
-        />
-        <DatabaseCard 
-        name = "My Database"
-        type = "PostgreSQL"
-        createdAt = "2024-06-01"
-        />
-        <DatabaseCard 
-        name = "My Database"
-        type = "PostgreSQL"
-        createdAt = "2024-06-01"
-        />
-        <DatabaseCard 
-        name = "My Database"
-        type = "PostgreSQL"
-        createdAt = "2024-06-01"
-        />
-        <DatabaseCard 
-        name = "My Database"
-        type = "PostgreSQL"
-        createdAt = "2024-06-01"
-        />
-        <DatabaseCard 
-        name = "My Database"
-        type = "PostgreSQL"
-        createdAt = "2024-06-01"
-        />
-        <DatabaseCard 
-        name = "My Database"
-        type = "PostgreSQL"
-        createdAt = "2024-06-01"
-        />
-        <DatabaseCard 
-        name = "My Database"
-        type = "PostgreSQL"
-        createdAt = "2024-06-01"
-        />
-        <DatabaseCard 
-        name = "My Database"
-        type = "PostgreSQL"
-        createdAt = "2024-06-01"
-        />
-        <DatabaseCard 
-        name = "My Database"
-        type = "PostgreSQL"
-        createdAt = "2024-06-01"
-        />
-        <DatabaseCard 
-        name = "My Database"
-        type = "PostgreSQL"
-        createdAt = "2024-06-01"
-        />
+        
         <DatabaseCard 
         name = "My Database"
         type = "PostgreSQL"
